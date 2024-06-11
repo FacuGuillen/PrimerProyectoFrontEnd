f3bff428f036288008ddd2defdb98043d28c4437
FacuGuillen <facu.guillen@hotmail.com>
2024-06-11 19:08:52 -0300

    Modificaciones de rutas en el index

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// const openLogin = document.getElementById('open-login')
+// const user = document.getElementById('user')
+// const openImg = document.getElementById('login-img')
+
+// document.addEventListener('click', function(e) {
+//     if ((!openLogin.contains(e.target) && !openImg.contains(e.target)) || user.contains(e.target)) {
+//         user.classList.add('d-none');
+//         user.classList.remove("animate__animated")
+//         user.classList.remove("animate__fadeIn")
+//     }
+// });
+
+// openLogin.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     user.classList.toggle("d-none");
+//     user.classList.toggle("animate__animated")
+//     user.classList.toggle("animate__fadeIn")
+// });
+
+// openImg.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     user.classList.toggle('d-none');
+//     user.classList.toggle("animate__animated")
+//     user.classList.toggle("animate__fadeIn")
+// });
+
+// user.addEventListener('click', function(e) {
+//     e.stopPropagation();
+// });
+
+
+
+// Slider del Inicio
+document.addEventListener('DOMContentLoaded', function() {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:1500,
+        dots: true
+    });
+});
